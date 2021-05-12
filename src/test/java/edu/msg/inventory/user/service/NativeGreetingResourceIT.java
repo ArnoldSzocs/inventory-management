5068eb9af04fc069d1d44798331615081ede3203
Szocs Arnold <arnoldszocs@yahoo.com>
2021-05-12 09:24:26 +0300

    Ãdeleted unnecessary files

--- src/test/java/edu/msg/inventory/user/service/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package edu.msg.inventory.user.service;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
